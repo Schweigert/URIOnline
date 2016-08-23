f31f9b27f9a45fae473afc2d271d27f24e8f6870
fleyhe <fleyhe0@gmail.com>
2016-08-23 19:36:47 -0300

    1768.py

---
+++ 1768.py
@@ -0,0 +1,14 @@
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1768
+from sys import stdin
+
+while True:
+    line = stdin.readline()
+    if not line:
+       break
+    a = int(line)
+    #Imprimir a linha central
+    for i in range(1,a+1,2):
+        print(" "*((a/2)-(i/2)) + "*"*i)
+
+    print(" "*(a/2) + "*")
+    print(" "*((a/2) -1) +"***")
